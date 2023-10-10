35891fc22da8d743c83657c3dc1972296fd7b1c6
Ramos <1joaoramos1@gmail.com>
2023-10-10 18:29:11 +0100

    Created Game Sketch

---
+++ data.pl
@@ -0,0 +1,44 @@
+% Board Starting Info
+board(13, 14).
+
+piece(square, 3, 4, stone).
+piece(square, 4, 4, stone).
+piece(square, 5, 4, stone).
+piece(square, 6, 4, stone).
+piece(square, 7, 4, stone).
+piece(square, 8, 4, stone).
+piece(square, 9, 4, stone).
+piece(square, 3, 5, stone).
+piece(square, 4, 5, stone).
+piece(square, 5, 5, stone).
+piece(square, 6, 5, stone).
+piece(square, 7, 5, stone).
+piece(square, 8, 5, stone).
+piece(square, 9, 5, stone).
+
+piece(circle, 3, 8, stone).
+piece(circle, 4, 8, stone).
+piece(circle, 5, 8, stone).
+piece(circle, 6, 8, stone).
+piece(circle, 7, 8, stone).
+piece(circle, 8, 8, stone).
+piece(circle, 9, 8, stone).
+piece(circle, 3, 9, stone).
+piece(circle, 4, 9, stone).
+piece(circle, 5, 9, stone).
+piece(circle, 6, 9, stone).
+piece(circle, 7, 9, stone).
+piece(circle, 8, 9, stone).
+piece(circle, 9, 9, stone).
+
+score(square, 4, 10).
+score(square, 5, 10).
+score(square, 6, 10).
+score(square, 7, 10).
+score(square, 8, 10).
+
+score(circle, 4, 3).
+score(circle, 5, 3).
+score(circle, 6, 3).
+score(circle, 7, 3).
+score(circle, 8, 3).
--- demo.pl
+++ demo.pl
@@ -51,19 +51,59 @@ start :- gameLoop(start).
 */
 
 :- use_module(library(random)).
+:- consult('data.pl').  
+
+switch_turn(square, circle).
+switch_turn(circle, square).
+
+get_input(Input) :-
+    get_code(C), skip_line,
+    Input is C - 48.
+
+gameLoop(T, piece_move, river) :-
+    write('| Pick a move\n'), 
+    write('1. Move\n'),
+    write('2. Flip\n'),
+    write('3. Rotate\n'),
+    write('Enter: '),
+    get_input(M),
+    write('Move: '), write(M), nl,
+    switch_turn(T, T2),
+    gameLoop(T2, piece_pick).
+
+gameLoop(T, piece_move, stone) :-
+    write('| Pick a move\n'), 
+    write('1. Move\n'),
+    write('2. Flip\n'),
+    write('Enter: '),
+    get_input(M),
+    write('Move: '), write(M), nl,
+    switch_turn(T, T2),
+    gameLoop(T2, piece_pick).
+
+gameLoop(T, piece_pick) :-
+    write('Turn: '), write(T), nl,
+    write('| Pick a piece\n'), 
+    write('Enter X: '),
+    get_input(X),
+    write('Enter Y: '),
+    get_input(Y),
+    piece(T, X, Y, S),
+    write('Piece state: '), write(S), nl,
+    gameLoop(T, piece_move, S).    
 
 start(T) :- 
-    write('Game Start'), nl,
-    write('Game Over'), nl.
+    write('Game Started'), nl, nl,
+    gameLoop(T, piece_pick).
 
-pick_turn(0, 'Square').
-pick_turn(1, 'Circle').
+random_turn(0, square).
+random_turn(1, circle).
 
-pick_turn(T) :-
+random_turn(T) :-
     random(0,2,P),
-    pick_turn(P, T).
+    random_turn(P, T).
 
 play :-
-    pick_turn(Turn),
+    random_turn(Turn),
     write(Turn), write(' starts'), nl,
     start(Turn).
