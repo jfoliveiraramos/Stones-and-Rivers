d89254ca6b919c0247674869accd5b69b5a4c103
Ramos <1joaoramos1@gmail.com>
2023-10-29 19:01:38 +0000

    Flipping Stones

--- data.pl
+++ data.pl
@@ -1,21 +1,29 @@
 :- dynamic board/1.
 
+belongs_to(squareStn, player_a).
+belongs_to(squareVrt, player_a).
+belongs_to(squareHrz, player_a).
+
+belongs_to(circleStn, player_b).
+belongs_to(circleVrt, player_b).
+belongs_to(circleHrz, player_b).
+
 board([
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, squareScr, squareScr, squareScr, squareScr, squareScr, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, emptySlot, emptySlot, emptySlot],
-    [emptySlot, circleVrt, emptySlot, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, emptySlot, emptySlot, emptySlot],
+    [emptySlot, emptySlot, emptySlot, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, circleStn, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
-    [emptySlot, emptySlot, squareHrz, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
+    [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, squareStn, squareStn, squareStn, squareStn, squareStn, squareStn, squareStn, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, squareStn, squareStn, squareStn, squareStn, squareStn, squareStn, squareStn, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, circleScr, circleScr, circleScr, circleScr, circleScr, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot],
     [emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot, emptySlot]
-]).
+]) :- !.
 
 board_size(Board, Width, Height) :-
     Board = [Row | _],
--- game.pl
+++ game.pl
@@ -1,10 +1,96 @@
 :- ensure_loaded('draw.pl').
+:- use_module(library(random)).
+:- use_module(library(lists)).
 
 % # Play
 
-play :- 
+turn(player_a, 0, 'Player A turn.\n').
+turn(player_b, 1, 'Player B turn.\n').
+
+decide_first_turn(T) :-
+    random(0, 2, R),
+    turn(T, R, _).
+
+swicth_turn(player_a, player_b).
+swicth_turn(player_b, player_a).
+
+piece_in(X, Y, Board, Piece) :-
+    nth0(Y, Board, Row),
+    nth0(X, Row, Piece).
+
+validate_piece(Turn, Piece):- belongs_to(Piece, Turn), !.
+validate_piece(Turn, Piece):-
+    \+ belongs_to(Piece, Turn),
+    repeat,
+    write('Invalid! Provive a valid X-Y\n'),
+    read(X-Y),
+    board(Board),
+    piece_in(X, Y, Board, Piece1),
+    belongs_to(Piece1, Turn).
+
+flip(squareStn, horizontal, squareHrz) :- !.
+flip(squareStn, veritcal, squareVrt) :- !.
+flip(circleStn, horizontal, circleHrz) :- !.
+flip(circleStn, veritcal, circleVrt) :- !.
+flip(squareHrz, _, squareStn) :- !.
+flip(squareVrt, _, squareStn) :- !.
+flip(circleHrz, _, circleStn) :- !.
+flip(circleVrt, _, circleStn) :- !.
+
+move_option(1, move).
+move_option(2, flip).
+
+select_stone_move(Move) :-
+    write('Select a move for the '),
+    write(' stone:\n'),
+    write('1 - Move\n'),
+    write('2 - Flip\n'),
+    read(Option),
+    move_option(Option, Move).
+
+select_move(squareStn, Move) :- select_stone_move(Move).
+select_move(circleStn, Move) :- select_stone_move(Move).
+
+execute_move(move, X, Y, _Piece) :-
+    write('Provide the coordinates of the destination, as follows: X-Y\n'),
+    read(X1-Y1),
+    board(Board),
+    board_size(Board, W, H),
+    X1 < W, X1 >= 0, 
+    Y1 < H, Y1 >= 0,
+    %only ortogonal moves
+    (X1 =:= X, Y1 =:= Y + 1; X1 =:= X, Y1 =:= Y - 1; X1 =:= X + 1, Y1 =:= Y; X1 =:= X - 1, Y1 =:= Y),
+    piece_in(X1, Y1, Board, emptySlot),
+    write('Valid Move!\n').
+
+execute_move(flip, X, Y, Piece) :-
+    flip(Piece, _, NewPiece),
+    board(Board),
+    nth0(Y, Board, Row, RestBoard),
+    nth0(X, Row, Piece, RestRow), 
+    nth0(X, NewRow, NewPiece, RestRow),
+    nth0(Y, NewBoard, NewRow, RestBoard),
+    retract(board(Board) :- !),
+    asserta(board(NewBoard) :- !).
+
+game_loop(Turn) :-
     board(B),
-    draw(B).
+    draw(B),
+    turn(Turn, _, TurnText),
+    write(TurnText),
+    write('Provide the coordinates of the piece you wish to play, as follows: X-Y\n'),
+    read(X-Y),
+    piece_in(X, Y, B, Piece),
+    validate_piece(Turn, Piece),
+    select_move(Piece, Move),
+    execute_move(Move, X, Y, Piece),
+    swicth_turn(Turn, NewTurn), 
+    game_loop(NewTurn).
+
+play :- 
+    decide_first_turn(T),
+    game_loop(T).
+    
 
 
 
